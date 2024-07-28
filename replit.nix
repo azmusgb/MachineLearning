272d26c0404d3f37e8921a276edee65c6e7df987
azmusgb <36898133+azmusgb@users.noreply.github.com>
2024-07-28 17:11:39 +0000

    New

--- replit.nix
+++ replit.nix
@@ -1,33 +1,18 @@
+# replit.nix
 { pkgs }: {
   deps = [
-    pkgs.python310Full
-    pkgs.python310Packages.pip
-    pkgs.python310Packages.numpy
-    pkgs.python310Packages.opencv4
-    pkgs.python310Packages.pillow
-    pkgs.python310Packages.matplotlib
-    pkgs.python310Packages.scikitlearn
-    pkgs.python310Packages.tqdm
-    pkgs.python310Packages.joblib
-    pkgs.libGL
-    pkgs.libGLU
-    pkgs.glib
-    pkgs.xorg.libX11
-    pkgs.xorg.libXext
-    pkgs.xorg.libXrender
-    pkgs.xorg.libXi
-    pkgs.xorg.libXfixes
-    pkgs.xorg.libXcursor
+    pkgs.python3
+    pkgs.python3Packages.pip
+    pkgs.python3Packages.pytorch
+    pkgs.python3Packages.torchvision
+    pkgs.python3Packages.opencv4
+    pkgs.python3Packages.numpy
+    pkgs.python3Packages.pillow
+    pkgs.python3Packages.scikit-learn
+    pkgs.python3Packages.matplotlib
+    pkgs.python3Packages.langdetect
+    pkgs.python3Packages.imgaug
+    pkgs.python3Packages.easyocr
+    pkgs.python3Packages.logging
   ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      pkgs.glib
-      pkgs.libGL
-      pkgs.libGLU
-    ];
-    PYTHONBIN = "${pkgs.python310Full}/bin/python3.10";
-    LANG = "en_US.UTF-8";
-  };
 }
